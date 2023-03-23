092bb9a360c2af06c0d0bb93fb9be0da22218fd1
Bibhabasu <bibhabasucvsc@kgpian.iitkgp.ac.in>
2023-03-23 11:54:21 +0530

    feed back form changed

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+  let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+// let p=document.getElementById("heading");
+// p.style.color="red";
+
+
+
+
